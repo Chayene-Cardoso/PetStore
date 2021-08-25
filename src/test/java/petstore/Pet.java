3dc2e6c98a0dd79e43acef3bde4e710010c36910
chayene.n.cardoso <chayene.natany@gmail.com>
2021-08-25 17:01:11 -0300

    Completei o script consultarPet (GET)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 
@@ -20,7 +19,7 @@ public class Pet {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
 
     }
-    @Test
+    @Test(priority = 1)
     public void incluirPet() throws IOException {
         String jsonBory = lerJson("db/pet1.json");
 
@@ -31,18 +30,39 @@ public class Pet {
                 .body(jsonBory)
         .when()//quando
                 .post(uri)
-        .then()//ent√£o
+        .then()//ent„o
                 .log().all()
                 .statusCode(200)
                 .body("name", is("luke"))
                 .body("status", is("available"))
-                .body("category.name",is("dog"))
-                .body("tags.name", contains("sta"));
-
-
-
-
-
+                .body("category.name",is("AX2345LORD"))
+                .body("tags.name", contains("data"));
     }
+
+    @Test(priority = 2)
+    public void consultarPet(){
+
+        String petId = "9643065515";
+
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("luke"))
+                .body("category.name", is("AX2345LORD"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+        ;
+
+                System.out.println("O token È " + token);
+    }
+
+
 
 }
