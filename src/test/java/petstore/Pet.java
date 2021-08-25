2092404fe4fb664eea3bec1a2c1495031934bbd5
chayene.n.cardoso <chayene.natany@gmail.com>
2021-08-25 15:47:22 -0300

    Completei o script incluirPet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
 
@@ -21,6 +24,7 @@ public class Pet {
     public void incluirPet() throws IOException {
         String jsonBory = lerJson("db/pet1.json");
 
+
         given()//dado
                 .contentType("application/json")  // comum em API REST - antigas usam "test/xml"
                 .log().all()
@@ -29,7 +33,11 @@ public class Pet {
                 .post(uri)
         .then()//então
                 .log().all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", is("luke"))
+                .body("status", is("available"))
+                .body("category.name",is("dog"))
+                .body("tags.name", contains("sta"));
 
 
 
